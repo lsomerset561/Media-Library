7b9c680afdd5929f6cc62d2f34b04c91e72d6b81
lsomerset561 <somerset.lindsey@gmail.com>
2016-03-13 11:01:14 -0400

    add details.php & id to HTML string; moved txt file

--- inc/functions.php
+++ inc/functions.php
@@ -5,8 +5,9 @@
 
  //function is separate to use on other pages
 function get_item_html($id, $item) {
-  $output = "<li><a href='#'><img src ='" 
-    . $item["img"] . "' alt= '" 
+  $output = "<li><a href='details.php?id="
+    . $id."'><img src='" 
+    . $item["img"] . "' alt='" 
     . $item["title"] . "' />" 
     . "<p>View Details</p>" 
     . "</a></li>";
